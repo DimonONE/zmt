cd243095b173c5e7ce7cd10a4c7426a3f4afb362
Dimon Beyba <dmezey@mail.ua>
2021-02-17 01:49:45 +0300

    headers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Header from './progect_files/Headers';
+import Section from './progect_files/Sections';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Section />
     </div>
   );
 }
+
+
+
 
 export default App;
---
+++ src/progect_files/Sections.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Sections.module.css";
+
+let Section = () => {
+    return(
+        <div className={styles.section}>
+            <h1>Helow</h1>
+        </div>
+    );
+}
+
+export default Section;
